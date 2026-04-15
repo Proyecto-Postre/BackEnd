0e551acc317b4f3f8868edd8482ac89510c5a515
Jafeth <u202314715@upc.edu.pe>
2026-04-15 00:15:01 -0500

    .....

--- DulceFe.API/Program.cs
+++ DulceFe.API/Program.cs
@@ -104,7 +104,14 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     {
         // Use hardcoded version to avoid SIGSEGV in some Linux environments during AutoDetect
         var serverVersion = new MySqlServerVersion(new Version(8, 0, 40)); 
-        options.UseMySql(connectionString, serverVersion);
+        options.UseMySql(connectionString, serverVersion, mysqlOptions => 
+        {
+            mysqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 5,
+                maxRetryDelay: TimeSpan.FromSeconds(30),
+                errorNumbersToAdd: null
+            );
+        });
     }
 });
 
