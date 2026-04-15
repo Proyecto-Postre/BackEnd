823334a5466714a538fa437bac42a17330760716
Jafeth <u202314715@upc.edu.pe>
2026-04-14 23:43:08 -0500

    defeat: corrected errors

--- DulceFe.API/IAM/Interfaces/REST/Resources/AuthResources.cs
+++ DulceFe.API/IAM/Interfaces/REST/Resources/AuthResources.cs
@@ -2,6 +2,6 @@ namespace DulceFe.API.IAM.Interfaces.REST.Resources;
 
 public record SignUpResource(string Username, string Email, string Password, string FirstName = "", string LastName = "", string Phone = "");
 public record SignInResource(string UsernameOrEmail, string Password);
-public record AuthenticatedUserResource(int Id, string Username, string Token);
+public record AuthenticatedUserResource(int Id, string Username, string Token, string Role = "user");
 public record ForgotPasswordResource(string Email);
 public record ResetPasswordResource(string Token, string NewPassword);
